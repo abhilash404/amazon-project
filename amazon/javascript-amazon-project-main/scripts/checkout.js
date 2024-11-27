d5ec06cdca1c0774cb9811b70b159a8000c102b6
abhilashpraharaj122@gmail.com <abhilashpraharaj122@gmail.com>
2024-11-27 15:57:10 +0530

    delete button made fuctional

--- amazon/javascript-amazon-project-main/scripts/checkout.js
+++ amazon/javascript-amazon-project-main/scripts/checkout.js
@@ -1,4 +1,4 @@
-import { cart } from "../data/cart.js";
+import { cart, removeFromCart } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatcurrency } from "./util/money.js";
 
@@ -36,7 +36,7 @@ cart.forEach((cartItem)=>{
                   <span class="update-quantity-link link-primary">
                     Update
                   </span>
-                  <span class="delete-quantity-link link-primary">
+                  <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
                     Delete
                   </span>
                 </div>
@@ -49,7 +49,7 @@ cart.forEach((cartItem)=>{
                 <div class="delivery-option">
                   <input type="radio" checked
                     class="delivery-option-input"
-                    name="delivery-option-${matchingProduct.id}">
+                    name="delivery-option-">
                   <div>
                     <div class="delivery-option-date">
                       Tuesday, June 21
@@ -92,3 +92,12 @@ cart.forEach((cartItem)=>{
 
 document.querySelector('.js-order-summary').innerHTML = cartSummaryHtml;
 console.log(cartSummaryHtml);
+
+document.querySelectorAll('.js-delete-link')
+  .forEach((link) => {
+    link.addEventListener('click',() => {
+      const productId= link.dataset.productId;
+      removeFromCart(productId);
+      console.log("delete");
+    });
+  })
